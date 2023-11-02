d38a84962ee33e4e20cadd680f44e1b60b8dbc3a
Stepan Didurenko <didurenkos@gmail.com>
2023-11-02 03:15:08 +0300

    Added logging to PopularCommandExecutor

--- src/main/java/edu/hw2/Task3/PopularCommandExecutor.java
+++ src/main/java/edu/hw2/Task3/PopularCommandExecutor.java
@@ -1,8 +1,12 @@
 package edu.hw2.Task3;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public final class PopularCommandExecutor {
     private final ConnectionManager manager;
     private final int maxAttempts;
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public PopularCommandExecutor(final ConnectionManager manager, final int maxAttempts) {
         this.manager = manager;
@@ -16,11 +20,13 @@ public final class PopularCommandExecutor {
     public void tryExecute(String command) throws ConnectionException {
         final var connection = manager.getConnection();
         for (int attemptIdx = 0; attemptIdx < this.maxAttempts; attemptIdx++) {
+            LOGGER.info("Attempt #" + attemptIdx);
             try {
                 connection.execute(command);
             } catch (ConnectionException cause) {
                 if (attemptIdx == this.maxAttempts - 1) {
                     connection.close();
+                    LOGGER.error("Too many attempts");
                     throw cause;
                 }
             }
