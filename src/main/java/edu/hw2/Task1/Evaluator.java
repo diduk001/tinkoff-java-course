72d449f6c5db06671bfed5147bfa206b7994c7d7
Stepan Didurenko <didurenkos@gmail.com>
2023-11-02 03:10:58 +0300

    Separated records to different files

--- src/main/java/edu/hw2/Task1/Evaluator.java
+++ src/main/java/edu/hw2/Task1/Evaluator.java
@@ -1,40 +1,6 @@
 package edu.hw2.Task1;
 
-public sealed interface Evaluator {
+public sealed interface Evaluator permits Constant, Addition, Exponent, Multiplication,
+    Negate {
     double evaluate();
-
-    record Constant(double val) implements Evaluator {
-        @Override
-        public double evaluate() {
-            return val;
-        }
-    }
-
-    record Negate(Evaluator expr) implements Evaluator {
-        @Override
-        public double evaluate() {
-            return -expr.evaluate();
-        }
-    }
-
-    record Exponent(Evaluator base, int pow) implements Evaluator {
-        @Override
-        public double evaluate() {
-            return Math.pow(base.evaluate(), pow);
-        }
-    }
-
-    record Addition(Evaluator a, Evaluator b) implements Evaluator {
-        @Override
-        public double evaluate() {
-            return a.evaluate() + b.evaluate();
-        }
-    }
-
-    record Multiplication(Evaluator a, Evaluator b) implements Evaluator {
-        @Override
-        public double evaluate() {
-            return a.evaluate() * b.evaluate();
-        }
-    }
 }
