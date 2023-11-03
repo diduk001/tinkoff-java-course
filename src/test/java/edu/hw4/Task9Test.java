445c3860f2b6c27a6362bfabcdd12e42cef376c9
Stepan Didurenko <didurenkos@gmail.com>
2023-11-03 14:00:23 +0300

    Added tests for task 9

---
+++ src/test/java/edu/hw4/Task9Test.java
@@ -0,0 +1,27 @@
+package edu.hw4;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static edu.hw4.SampleAnimals.SAMPLE_ANIMAL_LIST;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task9Test {
+    @Test
+    @DisplayName("Тест задания 1 на примере")
+    void sampleListTest() {
+        final List<Animal> sampleList = new ArrayList<>(SAMPLE_ANIMAL_LIST);
+        final Integer result = Main.pawsSum(sampleList);
+        final Integer expected = 2 * 4 + 2 * 4 + 2 * 2 + 2 * 8;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Пустой список")
+    void emptyListTest() {
+        final List<Animal> emptyList = new ArrayList<>();
+        final Integer result = Main.pawsSum(emptyList);
+        assertThat(result).isZero();
+    }
+}
