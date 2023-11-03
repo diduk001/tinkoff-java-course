5704c8dc8ab83b76219c86dd224b1cacc706150e
Stepan Didurenko <didurenkos@gmail.com>
2023-11-04 01:10:30 +0300

    Added tests for task 13

---
+++ src/test/java/edu/hw4/Task13Test.java
@@ -0,0 +1,50 @@
+package edu.hw4;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static edu.hw4.SampleAnimals.SAMPLE_ANIMAL_LIST;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task13Test {
+    @Test
+    @DisplayName("Тест задания 13 на примере")
+    void sampleListTest() {
+        final List<Animal> sampleList = new ArrayList<>(SAMPLE_ANIMAL_LIST);
+        final List<Animal> result = Main.animalsWithNameOfMoreThanTwoWords(sampleList);
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Непустой ответ")
+    void filterElementTest() {
+        final Animal oneWordName = new Animal(Animal.Type.CAT, "Name",
+            Animal.Sex.M, 10, 10, 10, false
+        );
+        final Animal twoWordsName = new Animal(Animal.Type.CAT, "Long Name",
+            Animal.Sex.M, 10, 10, 10, false
+        );
+        final Animal threeWordsName = new Animal(Animal.Type.CAT, "Very Long Name",
+            Animal.Sex.M, 10, 10, 10, false
+        );
+        final Animal fourWordsName = new Animal(Animal.Type.CAT, "Very Very Long Name",
+            Animal.Sex.M, 10, 10, 10, false
+        );
+
+        final List<Animal> animalList =
+            new ArrayList<>(List.of(oneWordName, twoWordsName, threeWordsName, fourWordsName));
+
+        final List<Animal> result = Main.animalsWithNameOfMoreThanTwoWords(animalList);
+        final List<Animal> expected = new ArrayList<>(List.of(threeWordsName, fourWordsName));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Пустой список")
+    void emptyListTest() {
+        final List<Animal> emptyList = new ArrayList<>();
+        final List<Animal> result = Main.animalsWithNameOfMoreThanTwoWords(emptyList);
+        assertThat(result).isEmpty();
+    }
+}
