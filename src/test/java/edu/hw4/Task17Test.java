f4310540eb832062620c7a79d7b9737e3bc6907d
Stepan Didurenko <didurenkos@gmail.com>
2023-11-04 02:22:52 +0300

    Added tests for task 17

---
+++ src/test/java/edu/hw4/Task17Test.java
@@ -0,0 +1,81 @@
+package edu.hw4;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static edu.hw4.SampleAnimals.BARBOS;
+import static edu.hw4.SampleAnimals.BELKA;
+import static edu.hw4.SampleAnimals.DORY;
+import static edu.hw4.SampleAnimals.MARY_JANE;
+import static edu.hw4.SampleAnimals.MURKA;
+import static edu.hw4.SampleAnimals.SAMPLE_ANIMAL_LIST;
+import static edu.hw4.SampleAnimals.SPIDEY;
+import static edu.hw4.SampleAnimals.TWEETIE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task17Test {
+    @Test
+    @DisplayName("Тест задания 17 на примере")
+    void sampleListTest() {
+        final List<Animal> sampleList = new ArrayList<>(SAMPLE_ANIMAL_LIST);
+        final Boolean result = Main.spidersBitesMoreThanDogs(sampleList);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("Тест с результатом true")
+    void trueTest() {
+        final List<Animal> angrySpidersList = new ArrayList<>(List.of(BARBOS, BELKA, SPIDEY, MARY_JANE, MARY_JANE));
+        final Boolean result = Main.spidersBitesMoreThanDogs(angrySpidersList);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("Тест с результатом false")
+    void falseTest() {
+        final List<Animal> angryDogsList = new ArrayList<>(List.of(BARBOS, BARBOS, BELKA, SPIDEY, MARY_JANE));
+        final Boolean result = Main.spidersBitesMoreThanDogs(angryDogsList);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("Одинаковый процент кусающихся собак и пауков")
+    void equalFrequencyTest() {
+        final List<Animal> equalFrequencyList = new ArrayList<>(List.of(BARBOS, MARY_JANE, MARY_JANE));
+        final Boolean result = Main.spidersBitesMoreThanDogs(equalFrequencyList);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("Тест без собак")
+    void noDogsTest() {
+        final List<Animal> noDogsList = new ArrayList<>(List.of(SPIDEY, MARY_JANE));
+        final Boolean result = Main.spidersBitesMoreThanDogs(noDogsList);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("Тест без пауков")
+    void noSpidersTest() {
+        final List<Animal> noSpidersList = new ArrayList<>(List.of(BELKA, BARBOS));
+        final Boolean result = Main.spidersBitesMoreThanDogs(noSpidersList);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("Тест без собак и пауков")
+    void noDogsAndSpidersTest() {
+        final List<Animal> noDogsAndSpidersList = new ArrayList<>(List.of(MURKA, DORY, TWEETIE));
+        final Boolean result = Main.spidersBitesMoreThanDogs(noDogsAndSpidersList);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("Пустой список")
+    void emptyListTest() {
+        final List<Animal> emptyList = new ArrayList<>();
+        final Boolean result = Main.spidersBitesMoreThanDogs(emptyList);
+        assertThat(result).isFalse();
+    }
+}
