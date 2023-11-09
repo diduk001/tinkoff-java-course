830b52258bf99a5e2f49d05a94745047c51dc913
Stepan Didurenko <didurenkos@gmail.com>
2023-11-09 19:29:59 +0300

    Completed task 5

---
+++ src/main/java/edu/hw5/CarNumberValidator.java
@@ -0,0 +1,16 @@
+package edu.hw5;
+
+import java.util.regex.Pattern;
+
+public final class CarNumberValidator {
+    private CarNumberValidator() {
+        throw new UnsupportedOperationException("This is a utility class");
+    }
+
+    public static boolean validateCarNumber(final String number) {
+        final Pattern numberPattern = Pattern.compile(
+            "^[АВЕКМНОРСТУХ]\\d\\d\\d[АВЕКМНОРСТУХ][АВЕКМНОРСТУХ]\\d{2,3}$"
+        );
+        return numberPattern.matcher(number).find();
+    }
+}
