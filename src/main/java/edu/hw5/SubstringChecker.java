618fa59b5184c2e91c004fe2b372944408d1757e
Stepan Didurenko <didurenkos@gmail.com>
2023-11-09 19:57:28 +0300

    Completed task 6

---
+++ src/main/java/edu/hw5/SubstringChecker.java
@@ -0,0 +1,14 @@
+package edu.hw5;
+
+import java.util.regex.Pattern;
+
+public final class SubstringChecker {
+    private SubstringChecker() {
+        throw new UnsupportedOperationException("This is a utility class");
+    }
+
+    public static boolean isSubstring(final String s, final String t) {
+        Pattern patternMatcher = Pattern.compile("^.*" + Pattern.quote(s) + ".*$");
+        return patternMatcher.matcher(t).find();
+    }
+}
---
+++ src/test/java/edu/hw5/Task6Test.java
@@ -0,0 +1,42 @@
+package edu.hw5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task6Test {
+    @Test
+    @DisplayName("Пример из условия")
+    void sampleTest() {
+        final boolean result = SubstringChecker.isSubstring("abc", "achfdbaabgabcaabg");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("Пустое S")
+    void emptySTest() {
+        final boolean result = SubstringChecker.isSubstring("", "abc");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("Пустое T")
+    void emptyTTest() {
+        final boolean result = SubstringChecker.isSubstring("abc", "");
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("Пустое S и T")
+    void emptySandTTest() {
+        final boolean result = SubstringChecker.isSubstring("", "");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("Одинаковые строки")
+    void equalStrings() {
+        final boolean result = SubstringChecker.isSubstring("abc", "abc");
+        assertThat(result).isTrue();
+    }
+}
