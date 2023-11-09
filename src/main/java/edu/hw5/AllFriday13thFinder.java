06f549d087a036221023c21152433b1f398d6ca4
Stepan Didurenko <didurenkos@gmail.com>
2023-11-09 18:41:54 +0300

    Completed task 2

---
+++ src/main/java/edu/hw5/AllFriday13thFinder.java
@@ -0,0 +1,24 @@
+package edu.hw5;
+
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class AllFriday13thFinder {
+    private AllFriday13thFinder() {
+        throw new UnsupportedOperationException("This is a utility class");
+    }
+
+    public static List<LocalDate> findAllFriday13thInYear(int year) {
+        final TemporalAdjuster adjuster = new NextFriday13thFinder();
+        ArrayList<LocalDate> result = new ArrayList<>();
+        LocalDate date = LocalDate.of(year, 1, 1).with(adjuster);
+        while (date.getYear() == year) {
+            result.add(date);
+            date = date.with(adjuster);
+        }
+
+        return result;
+    }
+}
