6d4b2da95c93fe323374f8a459a1d5d3ebb5cae1
Stepan Didurenko <didurenkos@gmail.com>
2023-11-10 21:32:29 +0300

    Completed task 8

--- src/main/java/edu/hw5/OneZeroRegex.java
+++ src/main/java/edu/hw5/OneZeroRegex.java
@@ -7,18 +7,57 @@ public final class OneZeroRegex {
         throw new UnsupportedOperationException("This is a utility class");
     }
 
+    // Task 7
+
     public static boolean lengthIsNoLessThan3AndThirdSymbolIs0(final String string) {
-        Pattern pattern = Pattern.compile("^[01]{2}0[01]*$");
+        final Pattern pattern = Pattern.compile("^[01]{2}0[01]*$");
         return pattern.matcher(string).find();
     }
 
     public static boolean startsAndEndsWithOneSymbol(final String string) {
-        Pattern pattern = Pattern.compile("(^[01]$)|(^([01])[01]*\\3$)");
+        final Pattern pattern = Pattern.compile("(^[01]$)|(^([01])[01]*\\3$)");
         return pattern.matcher(string).find();
     }
 
     public static boolean lengthBetween1and3(final String string) {
-        Pattern pattern = Pattern.compile("^[01]{1,3}$");
+        final Pattern pattern = Pattern.compile("^[01]{1,3}$");
+        return pattern.matcher(string).find();
+    }
+
+    // Task 8
+
+    public static boolean oddLength(final String string) {
+        final Pattern pattern = Pattern.compile("^[01]([01][01])*$");
+        return pattern.matcher(string).find();
+    }
+
+    public static boolean oddLengthZeroOrEvenLengthOne(final String string) {
+        final Pattern pattern = Pattern.compile("(^0([01][01])*$)|(^1[01]([01][01])*$)");
+        return pattern.matcher(string).find();
+    }
+
+    public static boolean zeroCountDividesByThree(final String string) {
+        final Pattern pattern = Pattern.compile("^1*(01*?01*?01*?)*$");
+        return pattern.matcher(string).find();
+    }
+
+    public static boolean no11And111(final String string) {
+        final Pattern pattern = Pattern.compile("^(?!11$|111$)[01]*$");
+        return pattern.matcher(string).find();
+    }
+
+    public static boolean oddSymbolsAre1(final String string) {
+        final Pattern pattern = Pattern.compile("^1([01]1)*[01]?$");
+        return pattern.matcher(string).find();
+    }
+
+    public static boolean notLessThanTwoZeroesAndNoMoreThanOneOne(final String string) {
+        final Pattern pattern = Pattern.compile("^0*?(000|100|010|001)?0*$");
+        return pattern.matcher(string).find();
+    }
+
+    public static boolean noSequentialOnes(final String string) {
+        final Pattern pattern = Pattern.compile("^((?!11)[01])*$");
         return pattern.matcher(string).find();
     }
 }
---
+++ src/test/java/edu/hw5/Task8Test.java
@@ -0,0 +1,79 @@
+package edu.hw5;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task8Test {
+    @ParameterizedTest(name = "Строка подходит под 1-е выражение: \"{0}\"")
+    @ValueSource(strings = {"010", "1", "0", "11011"})
+    void matchesFirst(final String string) {
+        assertThat(OneZeroRegex.oddLength(string)).isTrue();
+    }
+
+    @ParameterizedTest(name = "Строка не подходит под 1-е выражение: \"{0}\"")
+    @ValueSource(strings = {"0011", "11", "2", "212", "1113", "000000", "some string"})
+    void notMatchesFirst(final String string) {
+        assertThat(OneZeroRegex.oddLength(string)).isFalse();
+    }
+
+    @ParameterizedTest(name = "Строка подходит под 2-е выражение: \"{0}\"")
+    @ValueSource(strings = {"010", "1111", "0", "110110"})
+    void matchesSecond(final String string) {
+        assertThat(OneZeroRegex.oddLengthZeroOrEvenLengthOne(string)).isTrue();
+    }
+
+    @ParameterizedTest(name = "Строка не подходит под 2-е выражение: \"{0}\"")
+    @ValueSource(strings = {"00", "1", "11111", "012", "11a1", "some string"})
+    void notMatchesSecond(final String string) {
+        assertThat(OneZeroRegex.oddLengthZeroOrEvenLengthOne(string)).isFalse();
+    }
+
+    @ParameterizedTest(name = "Строка подходит под 3-е выражение: \"{0}\"")
+    @ValueSource(strings = {"000", "1110010", "1010101", "110001000101010111"})
+    void matchesThird(final String string) {
+        assertThat(OneZeroRegex.zeroCountDividesByThree(string)).isTrue();
+    }
+
+    @ParameterizedTest(name = "Строка не подходит под 3-е выражение: \"{0}\"")
+    @ValueSource(strings = {"00", "10", "11110", "012", "11a1", "some string"})
+    void notMatchesThird(final String string) {
+        assertThat(OneZeroRegex.zeroCountDividesByThree(string)).isFalse();
+    }
+
+    @ParameterizedTest(name = "Строка подходит под 4-е выражение: \"{0}\"")
+    @ValueSource(strings = {"0", "1", "110", "1110", "011", "0111", "0101011101"})
+    void matchesFourth(final String string) {
+        assertThat(OneZeroRegex.no11And111(string)).isTrue();
+    }
+
+    @ParameterizedTest(name = "Строка не подходит под 4-е выражение: \"{0}\"")
+    @ValueSource(strings = {"11", "111", "012", "11a1", "some string"})
+    void notMatchesFourth(final String string) {
+        assertThat(OneZeroRegex.no11And111(string)).isFalse();
+    }
+
+    @ParameterizedTest(name = "Строка подходит под 5-е выражение: \"{0}\"")
+    @ValueSource(strings = {"1", "10", "1111", "1110", "1010101", "11111111111111"})
+    void matchesFifth(final String string) {
+        assertThat(OneZeroRegex.oddSymbolsAre1(string)).isTrue();
+    }
+
+    @ParameterizedTest(name = "Строка не подходит под 5-е выражение: \"{0}\"")
+    @ValueSource(strings = {"0", "10100", "110", "012", "11a1", "some string"})
+    void notMatchesFifth(final String string) {
+        assertThat(OneZeroRegex.oddSymbolsAre1(string)).isFalse();
+    }
+
+    @ParameterizedTest(name = "Строка подходит под 6-е выражение: \"{0}\"")
+    @ValueSource(strings = {"1", "0", "10", "1000001", "00000", "1010101", "00000000000001"})
+    void matchesSixth(final String string) {
+        assertThat(OneZeroRegex.noSequentialOnes(string)).isTrue();
+    }
+
+    @ParameterizedTest(name = "Строка не подходит под 6-е выражение: \"{0}\"")
+    @ValueSource(strings = {"11", "101100", "0110101", "012", "11a1", "some string"})
+    void notMatchesSixth(final String string) {
+        assertThat(OneZeroRegex.noSequentialOnes(string)).isFalse();
+    }
+}
