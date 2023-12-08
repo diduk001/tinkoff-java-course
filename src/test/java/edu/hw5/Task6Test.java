3abcdf5e5174ba8c4bba8bb36cef58f036b42198
Stepan Didurenko <didurenkos@gmail.com>
2023-12-08 21:30:36 +0300

    Made subsequence instead of substring in task 6

--- src/test/java/edu/hw5/Task6Test.java
+++ src/test/java/edu/hw5/Task6Test.java
@@ -1,6 +1,6 @@
 package edu.hw5;
 
-import edu.hw5.Task6.SubstringChecker;
+import edu.hw5.Task6.SubsequenceChecker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,35 +9,49 @@ public class Task6Test {
     @Test
     @DisplayName("Пример из условия")
     void sampleTest() {
-        final boolean result = SubstringChecker.isSubstring("abc", "achfdbaabgabcaabg");
+        final boolean result = SubsequenceChecker.isSubstring("abc", "achfdbaabgabcaabg");
         assertThat(result).isTrue();
     }
 
     @Test
     @DisplayName("Пустое S")
     void emptySTest() {
-        final boolean result = SubstringChecker.isSubstring("", "abc");
+        final boolean result = SubsequenceChecker.isSubstring("", "abc");
         assertThat(result).isTrue();
     }
 
     @Test
     @DisplayName("Пустое T")
     void emptyTTest() {
-        final boolean result = SubstringChecker.isSubstring("abc", "");
+        final boolean result = SubsequenceChecker.isSubstring("abc", "");
         assertThat(result).isFalse();
     }
 
     @Test
     @DisplayName("Пустое S и T")
     void emptySandTTest() {
-        final boolean result = SubstringChecker.isSubstring("", "");
+        final boolean result = SubsequenceChecker.isSubstring("", "");
         assertThat(result).isTrue();
     }
 
     @Test
     @DisplayName("Одинаковые строки")
     void equalStrings() {
-        final boolean result = SubstringChecker.isSubstring("abc", "abc");
+        final boolean result = SubsequenceChecker.isSubstring("abc", "abc");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("Тест на подпоследовательности")
+    void subsequenceTest() {
+        final boolean result = SubsequenceChecker.isSubstring("bd", "abcd");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("Большой ест на подпоследовательности")
+    void bigSubsequenceTest() {
+        final boolean result = SubsequenceChecker.isSubstring("test", "sttthhhellttsssalt");
         assertThat(result).isTrue();
     }
 }
