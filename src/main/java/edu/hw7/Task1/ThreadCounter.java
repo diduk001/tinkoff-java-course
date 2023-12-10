5c0f5e88bb65e4071e00da9b3cd0038eb2707b10
Stepan Didurenko <didurenkos@gmail.com>
2023-12-10 23:14:39 +0300

    Changed ArrayList<> to List<>

--- src/main/java/edu/hw7/Task1/ThreadCounter.java
+++ src/main/java/edu/hw7/Task1/ThreadCounter.java
@@ -1,6 +1,7 @@
 package edu.hw7.Task1;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,7 +21,7 @@ public final class ThreadCounter {
 
         AtomicInteger counter = new AtomicInteger(0);
 
-        ArrayList<Thread> threads = new ArrayList<>();
+        List<Thread> threads = new ArrayList<>();
         if (n <= MAX_THREADS) {
             for (int i = 0; i < n; i++) {
                 threads.add(new Incrementor(counter, 1));
